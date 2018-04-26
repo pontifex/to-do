d7b08b4f8a14aeeb54afdb9c229d47160672732c
pontifex <lucas.wawrzyniak@gmail.com>
2018-04-26 16:20:12 +0200

    feat(symfony) Symfony framework with basic configuration
    Based on https://github.com/symfony/demo

---
+++ symfony/src/Entity/Task.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\TaskRepository")
+ * @ORM\Table(name="todo_task")
+ */
+class Task
+{
+    public const NUM_ITEMS = 1;
+
+    /**
+     * @var int
+     *
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+}
