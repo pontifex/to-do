f567875e1f5a46e4faf8336aac1a74856f96fba4
pontifex <lucas.wawrzyniak@gmail.com>
2018-04-26 16:20:12 +0200

    feat(symfony) Symfony framework with basic configuration
    Based on https://github.com/symfony/demo

---
+++ symfony/src/Controller/TaskController.php
@@ -0,0 +1,53 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Controller;
+
+use App\Entity\Task;
+use App\Repository\TaskRepository;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Cache;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Symfony\Component\EventDispatcher\GenericEvent;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Controller used to manage tasks
+ */
+class TaskController extends AbstractController
+{
+    /**
+     * @Route("/", defaults={"page": "1", "_format"="html"}, name="task_index")
+     * @Route("/page/{page}", defaults={"_format"="html"}, requirements={"page": "[1-9]\d*"}, name="task_index_paginated")
+     * @Method("GET")
+     * @Cache(smaxage="10")
+     */
+    public function index(int $page, string $_format, TaskRepository $tasks): Response
+    {
+        $latestTasks = $tasks->findLatest($page);
+
+        return $this->render('task/index.'.$_format.'.twig', ['tasks' => $latestTasks]);
+    }
+
+    /**
+     * @Route("/tasks/{id}", name="task_show")
+     * @Method("GET")
+     */
+    public function taskShow(Task $task): Response
+    {
+        return $this->render('task/show.html.twig', ['task' => $task]);
+    }
+}
