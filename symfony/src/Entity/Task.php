081db057f9d317f9b2e7cba2743474aec5d9cb95
pontifex <lucas.wawrzyniak@gmail.com>
2018-04-26 18:06:32 +0200

    feat(task) Task entity with final properties + updated task templates

--- symfony/src/Entity/Task.php
+++ symfony/src/Entity/Task.php
@@ -1,17 +1,9 @@
 <?php
 
-/*
- * This file is part of the Symfony package.
- *
- * (c) Fabien Potencier <fabien@symfony.com>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\TaskRepository")
@@ -20,6 +12,10 @@ use Doctrine\ORM\Mapping as ORM;
 class Task
 {
     public const NUM_ITEMS = 1;
+
+    public const STATUS_TODO = 'Todo';
+    public const STATUS_IN_PROGRESS = 'In Progress';
+    public const STATUS_COMPLETED = 'Completed';
 
     /**
      * @var int
@@ -30,8 +26,81 @@ class Task
      */
     private $id;
 
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="string")
+     */
+    private $title;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="string")
+     */
+    private $description;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="string", columnDefinition="ENUM('Todo', 'In Progress', 'Completed')")
+     */
+    private $status;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(type="datetime")
+     * @Assert\DateTime
+     */
+    private $publishedAt;
+
     public function getId(): int
     {
         return $this->id;
+    }
+
+    public function setTitle(string $title): void
+    {
+        $this->title = $title;
+    }
+
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): void
+    {
+        $this->description = $description;
+    }
+
+    public function getStatus(): string
+    {
+        return $this->status;
+    }
+
+    public function setStatus(string $status): void
+    {
+        if (!in_array($status, array(self::STATUS_TODO, self::STATUS_IN_PROGRESS. self::STATUS_COMPLETED))) {
+            throw new \InvalidArgumentException("Invalid status");
+        }
+
+        $this->status = $status;
+    }
+
+    public function getPublishedAt(): \DateTime
+    {
+        return $this->publishedAt;
+    }
+
+    public function setPublishedAt(?\DateTime $publishedAt): void
+    {
+        $this->publishedAt = $publishedAt;
     }
 }
