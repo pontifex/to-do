3a73e8d09d7536c292570fd572a67a9c57d6648c
pontifex <lucas.wawrzyniak@gmail.com>
2018-04-27 01:05:02 +0200

    feat(task) Add Task
    Adding translation fields not done yet!

---
+++ symfony/src/Form/TaskType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Task;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TaskType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title', null, [
+                'attr' => ['autofocus' => true],
+                'label' => 'label.title',
+            ])
+            ->add('description', TextareaType::class, [
+                'label' => 'label.description',
+            ])
+        ;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Task::class,
+        ]);
+    }
+}
